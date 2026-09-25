612297f101f4ca0c6cece2e8c59f8343eddb85ff
SUNIL GUPTA <guptasunil16927@gmail.com>
2026-09-26 00:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (84.36%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int n = nums.length;
+       int st =0,end =n-1;
+      
+
+       while(st<=end){
+             int mid = st+(end-st)/2;
+            if(target == nums[mid]) return mid;
+
+            else if(target > nums[mid]) st = mid+1;
+
+            else{
+                end = mid-1;
+            } 
+        }
+        return -1;
+    }
+}
